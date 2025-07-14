d64f7f9671c1ee9ebd1d56e061b81a7ffc5ff061
KMD-Vignesh <KMD.Vignesh@outlook.com>
2025-07-14 13:04:39 +0530

    rework project

--- crmapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", view=views.index, name="index"),
-    path("product", view=views.product, name="product"),
-
-]
--- crmapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the crm index now")
-
-def product(request):
-    return HttpResponse("Hello, world. You're at the crm product now")
